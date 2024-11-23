c9733580f2d6a1512c6a2dd160baa16a1df89132
Jennifer You <jen.g.s.you@gmail.com>
2024-11-23 06:29:26 -0500

    authentication

--- FLUTTER/car_rec_chatbot/lib/pages/home_page.dart
+++ FLUTTER/car_rec_chatbot/lib/pages/home_page.dart
@@ -7,7 +7,8 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Home")),
+      appBar: AppBar(
+        title: Text("Home")),
     );
   }
 }
