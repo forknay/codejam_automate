dc6a82ac25093b8602e4805ae7aacd7452afaa9b
DorySong <dorysongmtl@gmail.com>
2024-11-24 02:24:09 -0500

    Modified readability of cohere

--- CohereServer/data_collection.py
+++ CohereServer/data_collection.py
@@ -10,46 +10,47 @@ co = cohere.ClientV2(
  #   friendly manner and informatively recommending the user with a car model replaced with the variable X.
 #"""
 
-system_preset_input = """## Task and Context
-Reword the input as a message from a friendly customer who is inquiring about car characteristics for their requirements. Always mention your requirements 
-"""
-chat_log_input = [{'role': 'system', 'content': system_preset_input}]
-
 def generate_text_input(message):
     # Generate the response by streaming it
     bot_response = ""
     response = co.chat_stream(model="command-r-plus-08-2024",
                               messages=chat_log_input,
-                              max_tokens=32,
-                              temperature=0.7,
+                              max_tokens=30,
+                              temperature=0.5,
                               k=0,
                               p=0.9
                               )
     for event in response:
         if event.type == "content-delta":
             bot_response += event.delta.message.content.text
-            print(event.delta.message.content.text, end='')
-
-
-system_preset_output = """## Task and Context
-Reword the input as a message from a friendly salesman would give to a customer about a car. Mention the model you recommend.
-"""
-
-chat_log_output = [{'role': 'system', 'content': system_preset_output}]
+            return bot_response
+        
 def generate_text_output(message):
     # Generate the response by streaming it
     bot_response = ""
     response = co.chat_stream(model="command-r-plus-08-2024",
                               messages=chat_log_output,
-                              max_tokens=32,
-                              temperature=0.7,
+                              max_tokens=24,
+                              temperature=0.5,
                               k=0,
                               p=0.9
                               )
     for event in response:
         if event.type == "content-delta":
             bot_response += event.delta.message.content.text
-            print(event.delta.message.content.text, end='')
+            return bot_response
+
+#Setting up prompts and chat history for 2 chatbots that convert a robotic-like input into something more human
+system_preset_input = """## Task and Context
+Reword the input as a message from a very straight-forward customer who is inquiring about car characteristics for their requirements. Always mention your requirements. Very important sentences must be under 100 characters. 
+"""
+chat_log_input = [{'role': 'system', 'content': system_preset_input}]
+
+system_preset_output = """## Task and Context
+Reword the input as a message from a friendly, straight forward salesman would give to a customer about a car. Mention the model you recommend with information related to the customers inquiries. Very important sentences must be under 100 characters.
+"""
+chat_log_output = [{'role': 'system', 'content': system_preset_output}]
+
 
 message_input = "Hi!, I am looking for a car with Model CX-9, Engine_Block_Type I and PassengerCapacity 7."
 chat_log_input.append({'role': 'user', 'content': message_input})
@@ -58,4 +59,5 @@ message_output = "I would recommend the Levante (C120321)."
 chat_log_output.append({'role': 'user', 'content': message_output})
 
 input_generate = generate_text_input(message_input)
+print()
 output_generate = generate_text_output(message_output)
