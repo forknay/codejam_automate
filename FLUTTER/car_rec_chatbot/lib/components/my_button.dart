dddf8191c11c20db7b069eab6c76293f934fe6ff
julien <julienyang12@gmail.com>
2024-11-24 02:06:21 -0500

    pretty button

--- FLUTTER/car_rec_chatbot/lib/components/my_button.dart
+++ FLUTTER/car_rec_chatbot/lib/components/my_button.dart
@@ -1,27 +1,67 @@
 import 'package:flutter/material.dart';
 
-class MyButton extends StatelessWidget {
+class MyButton extends StatefulWidget {
   final void Function()? onTap;
   final String text;
 
-  const MyButton( {
-    super.key, 
-    required this.text, 
+  const MyButton({
+    super.key,
+    required this.text,
     required this.onTap,
   });
 
   @override
+  _MyButtonState createState() => _MyButtonState();
+}
+
+class _MyButtonState extends State<MyButton> {
+  bool _isHovered = false;
+
+  @override
   Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Theme.of(context).colorScheme.inversePrimary,
-        ),
-        padding: const EdgeInsets.all(25),
-        margin: const EdgeInsets.symmetric(horizontal: 25),
-        child: Center(
-          child: Text(text),
+    final theme = Theme.of(context);
+
+    return MouseRegion(
+      onEnter: (_) => setState(() => _isHovered = true),
+      onExit: (_) => setState(() => _isHovered = false),
+      child: GestureDetector(
+        onTap: widget.onTap,
+        child: AnimatedContainer(
+          duration: const Duration(milliseconds: 200),
+          width: _isHovered ? 220 : 200,
+          height: _isHovered ? 70 : 60,
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: _isHovered
+                  ? [Theme.of(context).colorScheme.inversePrimary, Theme.of(context).colorScheme.primary]
+                  : [Theme.of(context).colorScheme.primary, Theme.of(context).colorScheme.inversePrimary],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+            boxShadow: _isHovered
+                ? [
+                    BoxShadow(
+                      color: Colors.black.withOpacity(0.2),
+                      spreadRadius: 2,
+                      blurRadius: 5,
+                      offset: Offset(0, 3),
+                    ),
+                  ]
+                : [],
+            borderRadius: BorderRadius.circular(12),
+          ),
+          padding: const EdgeInsets.all(8),
+          margin: const EdgeInsets.symmetric(horizontal: 25),
+          child: Center(
+            child: Text(
+              widget.text,
+              style: TextStyle(
+                color: theme.colorScheme.onPrimary,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
         ),
       ),
     );
